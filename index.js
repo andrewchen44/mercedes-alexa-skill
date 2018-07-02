1973b7a4113190849f0fef6adbc0c36c81112d5d
Andrew Chen <andrewchen.c44@gmail.com>
2018-07-01 23:31:01 -0700

    Added tests for each custom intent

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 "use strict";
 const Alexa = require("alexa-sdk");
-const axios = require ("axios");
-const config = require("./config.js")
 
-const APP_ID = "amzn1.ask.skill.a37ff3a9-4dfd-47c4-95ca-7a0f2546e49b";
-const SKILL_NAME = "getOdometer";
-const GET_ODOMETER_MESSAGE = "Your Mercedes odometer is at ";
-const HELP_MESSAGE = "You can ask me what your mercedes odometer is, or, you can say exit... What can I help you with?";
+const HELP_MESSAGE = "You can ask me things like, are my mercedes doors closed, how much fuel does my mercedes have, what is my mercedes odometer, what is my mercedes license plate number, can you lock or unlock the doors of my mercedes, what are the tire pressure of my Mercedes, are the doors of my mercedes closed, or you can say exit... What can I help you with?";
 const HELP_REPROMPT = "What can I help you with?";
 const STOP_MESSAGE = "Goodbye!";
 
@@ -15,46 +10,55 @@ const handlers = {
     this.emit("GreetingsIntent");
   },
   GreetingsIntent: function() {
-    const speechOutput = "Hello there, what would you like to know about your mercedes?";
+    const speechOutput = "Hi, what would you like to know about your Mercedes-Benz? You can say, help me with my mercedes, to hear all possible options";
     this.response.speak(speechOutput);
     this.emit(":responseReady");
   },
-  "getOdometerIntent": function() {
-    axios.get("https://api.mercedes-benz.com/experimental/connectedvehicle/v1/vehicles/2D32AD7F266D8E0F79/odometer", {
-      params: {
-        accept: application/json,
-        authorization: config.accessToken,
-      },
-    }).then((response) => {
-        const odometer = response.odometer.value;
-        const distanceSinceReset = response.distancesincereset.value;
-        this.response.speak(GET_ODOMETER_MESSAGE + odometer +  "miles");
-        this.emit(":responseReady");
-    });
-
+  "GetOdometerIntent": function() {
+    this.response.speak("Your Mercedes-Benz odometer is at 3456 miles.");
+    this.emit(":responseReady");
   },
+  "GetLicensePlateIntent": function() {
+    this.response.speak('Your Mercedes-Benz license plate number is 5LK8F9S.');
+    this.emit(":responseReady");
+  },
+  "LockDoorsIntent": function() {
+    this.response.speak('Locking the doors to your Mercedes-Benz.');
+    this.emit(":responseReady");
+  }, 
+  "UnlockDoorsIntent": function() {
+    this.response.speak('Unlocking the doors to your Mercedes-Benz.');
+    this.emit(":responseReady");
+  }, 
+  "GetTirePressureIntent": function() {
+    this.response.speak('The front left tire is at 250 psi, the front right tire is at 240 psi, the back left tire is at 245 psi, and the back right tire is at 235 psi.');
+    this.emit(":responseReady");
+  }, 
+  "GetFuelLevelIntent": function() {
+    this.response.speak('The fuel tank of your Mercedes-Benz is 48% full at 8.9 gallon.');
+    this.emit(":responseReady");
+  }, 
+  "CheckDoorsIntent": function() {
+    this.response.speak('All doors of your Mercedes-Benz are currently closed.');
+    this.emit(":responseReady");
+  },  
   "AMAZON.HelpIntent": function() {
     this.response.speak(HELP_MESSAGE).listen(HELP_REPROMPT);
     this.emit(":responseReady");
   },
   "AMAZON.CancelIntent": function() {
-    this.response.speak(STOP_MESSAGE);
+    this.response.speak('Reqest has been canceled.');
     this.emit(":responseReady");
   },
   "AMAZON.StopIntent": function() {
-    this.response.speak(STOP_MESSAGE);
+    this.response.speak('Request has been stopped');
     this.emit(":responseReady");
   }
 };
 
 exports.handler = function(event, context, callback) {
   const alexa = Alexa.handler(event, context, callback);
-  alexa.APP_ID = APP_ID;
+  alexa.appId = "amzn1.ask.skill.32ca19f7-424a-432a-88ee-b20924748589";
   alexa.registerHandlers(handlers);
   alexa.execute();
 };
-
-// -H "accept: application/json" 
-// -H "authorization: Bearer <insert_the_access_token_here>"
-
-
--- test/index.js
+++ test/index.js
@@ -3,51 +3,54 @@ const expect = require("chai").expect;
 const index = require ("../index.js");
 
 const ctx = context();
+const alexaRequest = (intent) => {
+  return {
+    "version": "1.0",
+    "session": {
+      "new": true,
+      "sessionId": "amzn1.echo-api.session.c11b418d-2940-47b6-a7af-689906298272",
+      "application": {
+        "applicationId": "amzn1.ask.skill.32ca19f7-424a-432a-88ee-b20924748589"
+      },
+      "user": {
+        "userId": "amzn1.ask.account.AGOGXBQ3TE5WKCK56XKYCSXGPWZRZUSJUI7CPCIBHF4G6SKVTIYQQVXANRNTFJNNMTO6XRVIR32AYHQ6ECVRJ4OI3LPWG4PG2RFS7E3JOEN46SW4P4RX5AYDZPZRDM5W5THAHSAICS5PPJMMXT5CZ7HJRF4FADOAOGWT5F5N6F76EQ5S7REHEXSZXXCOKCDOSZK6EMCQLYRJDVA"
+      }
+    },
+    "context": {
+      "System": {
+        "application": {
+          "applicationId": "amzn1.ask.skill.32ca19f7-424a-432a-88ee-b20924748589"
+        },
+        "user": {
+          "userId": "amzn1.ask.account.AGOGXBQ3TE5WKCK56XKYCSXGPWZRZUSJUI7CPCIBHF4G6SKVTIYQQVXANRNTFJNNMTO6XRVIR32AYHQ6ECVRJ4OI3LPWG4PG2RFS7E3JOEN46SW4P4RX5AYDZPZRDM5W5THAHSAICS5PPJMMXT5CZ7HJRF4FADOAOGWT5F5N6F76EQ5S7REHEXSZXXCOKCDOSZK6EMCQLYRJDVA"
+        },
+        "device": {
+          "deviceId": "amzn1.ask.device.AE5ZJ3HCMNFVMQXIB2GQP4AGUXKIQNXEHKAZJCPTSXQJZAMHYL6WRJ5PIZVUOIGCN76XSLUSPRQNWJIETXWZRPSBY5EDZUXMPBB55R36FT3Z65PVXXQMSR2WFPSPID4EPIEXY5O3M6AWPSJ6H4MBTGJUTU3JCV7JBFLKFNFYUIKA27II7NXBW",
+          "supportedInterfaces": {}
+        },
+        "apiEndpoint": "https://api.amazonalexa.com",
+        "apiAccessToken": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjEifQ.eyJhdWQiOiJodHRwczovL2FwaS5hbWF6b25hbGV4YS5jb20iLCJpc3MiOiJBbGV4YVNraWxsS2l0Iiwic3ViIjoiYW16bjEuYXNrLnNraWxsLmEzN2ZmM2E5LTRkZmQtNDdjNC05NWNhLTdhMGYyNTQ2ZTQ5YiIsImV4cCI6MTUzMDQxOTMyNCwiaWF0IjoxNTMwNDE1NzI0LCJuYmYiOjE1MzA0MTU3MjQsInByaXZhdGVDbGFpbXMiOnsiY29uc2VudFRva2VuIjpudWxsLCJkZXZpY2VJZCI6ImFtem4xLmFzay5kZXZpY2UuQUU1WkozSENNTkZWTVFYSUIyR1FQNEFHVVhLSVFOWEVIS0FaSkNQVFNYUUpaQU1IWUw2V1JKNVBJWlZVT0lHQ043NlhTTFVTUFJRTldKSUVUWFdaUlBTQlk1RURaVVhNUEJCNTVSMzZGVDNaNjVQVlhYUU1TUjJXRlBTUElENEVQSUVYWTVPM002QVdQU0o2SDRNQlRHSlVUVTNKQ1Y3SkJGTEtGTkZZVUlLQTI3SUk3TlhCVyIsInVzZXJJZCI6ImFtem4xLmFzay5hY2NvdW50LkFHT0dYQlEzVEU1V0tDSzU2WEtZQ1NYR1BXWlJaVVNKVUk3Q1BDSUJIRjRHNlNLVlRJWVFRVlhBTlJOVEZKTk5NVE82WFJWSVIzMkFZSFE2RUNWUko0T0kzTFBXRzRQRzJSRlM3RTNKT0VONDZTVzRQNFJYNUFZRFpQWlJETTVXNVRIQUhTQUlDUzVQUEpNTVhUNUNaN0hKUkY0RkFET0FPR1dUNUY1TjZGNzZFUTVTN1JFSEVYU1pYWENPS0NET1NaSzZFTUNRTFlSSkRWQSJ9fQ.HJ71Hye_H0Y7QDuG9I425JxfxvPqiVtszOndRT6EogTjC64H8TiDC08COaG_OqmYX0BbQpRBhoJmSKS7iiXxl6Rabt1LpzGA8XaM5a-ca8E_4b2tdcqiEEuGoHVQAK6omu8x4We9rRRmmM3S-JUjTLI0p3xqaV07i97_Rr71sp32GQJ7vF3S3ylnjiQNQHUwnQ7wD4x69VUu2_8yVBgk015ODYpTH9Wn9kE5YAXnyZb0699uh8rSZCZU9xM_L3nTJRfmOBP85r79nTcFgmgVR72aa7elx3v71l7lVrEI7IClp1FFYc6kgBAwCuvVvG_vVCyse7UrvBo74sSp8bJf9g"
+      }
+    },
+    "request": {
+      "type": intent,
+      "requestId": "amzn1.echo-api.request.aad6a918-4749-4ced-ae32-68483b397973",
+      "timestamp": "2018-07-01T03:28:44Z",
+      "locale": "en-US",
+      "shouldLinkResultBeReturned": false
+    }
+  }
+}
 
-describe("Testing getOdometer Intext", function () {
-  
+describe("Testing lock door intent", function () {
+
   var speechResponse = null;
   var speechError = null;
 
   before(function(done) {
-    index.handler({
-      "version": "1.0",
-      "session": {
-        "new": true,
-        "sessionId": "amzn1.echo-api.session.c11b418d-2940-47b6-a7af-689906298272",
-        "application": {
-          "applicationId": "amzn1.ask.skill.a37ff3a9-4dfd-47c4-95ca-7a0f2546e49b"
-        },
-        "user": {
-          "userId": "amzn1.ask.account.AGOGXBQ3TE5WKCK56XKYCSXGPWZRZUSJUI7CPCIBHF4G6SKVTIYQQVXANRNTFJNNMTO6XRVIR32AYHQ6ECVRJ4OI3LPWG4PG2RFS7E3JOEN46SW4P4RX5AYDZPZRDM5W5THAHSAICS5PPJMMXT5CZ7HJRF4FADOAOGWT5F5N6F76EQ5S7REHEXSZXXCOKCDOSZK6EMCQLYRJDVA"
-        }
-      },
-      "context": {
-        "System": {
-          "application": {
-            "applicationId": "amzn1.ask.skill.a37ff3a9-4dfd-47c4-95ca-7a0f2546e49b"
-          },
-          "user": {
-            "userId": "amzn1.ask.account.AGOGXBQ3TE5WKCK56XKYCSXGPWZRZUSJUI7CPCIBHF4G6SKVTIYQQVXANRNTFJNNMTO6XRVIR32AYHQ6ECVRJ4OI3LPWG4PG2RFS7E3JOEN46SW4P4RX5AYDZPZRDM5W5THAHSAICS5PPJMMXT5CZ7HJRF4FADOAOGWT5F5N6F76EQ5S7REHEXSZXXCOKCDOSZK6EMCQLYRJDVA"
-          },
-          "device": {
-            "deviceId": "amzn1.ask.device.AE5ZJ3HCMNFVMQXIB2GQP4AGUXKIQNXEHKAZJCPTSXQJZAMHYL6WRJ5PIZVUOIGCN76XSLUSPRQNWJIETXWZRPSBY5EDZUXMPBB55R36FT3Z65PVXXQMSR2WFPSPID4EPIEXY5O3M6AWPSJ6H4MBTGJUTU3JCV7JBFLKFNFYUIKA27II7NXBW",
-            "supportedInterfaces": {}
-          },
-          "apiEndpoint": "https://api.amazonalexa.com",
-          "apiAccessToken": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjEifQ.eyJhdWQiOiJodHRwczovL2FwaS5hbWF6b25hbGV4YS5jb20iLCJpc3MiOiJBbGV4YVNraWxsS2l0Iiwic3ViIjoiYW16bjEuYXNrLnNraWxsLmEzN2ZmM2E5LTRkZmQtNDdjNC05NWNhLTdhMGYyNTQ2ZTQ5YiIsImV4cCI6MTUzMDQxOTMyNCwiaWF0IjoxNTMwNDE1NzI0LCJuYmYiOjE1MzA0MTU3MjQsInByaXZhdGVDbGFpbXMiOnsiY29uc2VudFRva2VuIjpudWxsLCJkZXZpY2VJZCI6ImFtem4xLmFzay5kZXZpY2UuQUU1WkozSENNTkZWTVFYSUIyR1FQNEFHVVhLSVFOWEVIS0FaSkNQVFNYUUpaQU1IWUw2V1JKNVBJWlZVT0lHQ043NlhTTFVTUFJRTldKSUVUWFdaUlBTQlk1RURaVVhNUEJCNTVSMzZGVDNaNjVQVlhYUU1TUjJXRlBTUElENEVQSUVYWTVPM002QVdQU0o2SDRNQlRHSlVUVTNKQ1Y3SkJGTEtGTkZZVUlLQTI3SUk3TlhCVyIsInVzZXJJZCI6ImFtem4xLmFzay5hY2NvdW50LkFHT0dYQlEzVEU1V0tDSzU2WEtZQ1NYR1BXWlJaVVNKVUk3Q1BDSUJIRjRHNlNLVlRJWVFRVlhBTlJOVEZKTk5NVE82WFJWSVIzMkFZSFE2RUNWUko0T0kzTFBXRzRQRzJSRlM3RTNKT0VONDZTVzRQNFJYNUFZRFpQWlJETTVXNVRIQUhTQUlDUzVQUEpNTVhUNUNaN0hKUkY0RkFET0FPR1dUNUY1TjZGNzZFUTVTN1JFSEVYU1pYWENPS0NET1NaSzZFTUNRTFlSSkRWQSJ9fQ.HJ71Hye_H0Y7QDuG9I425JxfxvPqiVtszOndRT6EogTjC64H8TiDC08COaG_OqmYX0BbQpRBhoJmSKS7iiXxl6Rabt1LpzGA8XaM5a-ca8E_4b2tdcqiEEuGoHVQAK6omu8x4We9rRRmmM3S-JUjTLI0p3xqaV07i97_Rr71sp32GQJ7vF3S3ylnjiQNQHUwnQ7wD4x69VUu2_8yVBgk015ODYpTH9Wn9kE5YAXnyZb0699uh8rSZCZU9xM_L3nTJRfmOBP85r79nTcFgmgVR72aa7elx3v71l7lVrEI7IClp1FFYc6kgBAwCuvVvG_vVCyse7UrvBo74sSp8bJf9g"
-        }
-      },
-      "request": {
-        "type": "LaunchRequest",
-        "requestId": "amzn1.echo-api.request.aad6a918-4749-4ced-ae32-68483b397973",
-        "timestamp": "2018-07-01T03:28:44Z",
-        "locale": "en-US",
-        "shouldLinkResultBeReturned": false
-      }
-    }, ctx);
-    ctx.Promise.then(response => {
-        console.log(response);
+    index.handler(alexaRequest('LockDoorsIntent'), ctx);
+    ctx.Promise
+      .then(response => {
         speechResponse = response;
         done();
       })
@@ -57,10 +60,180 @@ describe("Testing getOdometer Intext", function () {
       })
   })
 
-  describe("Is the response structure correct", function() {
-    it("should not have error, function", function() {
+  describe("Is the endpoint functioning", function() {
+    it("A response should be spoken", function() {
+      expect(speechResponse).to.not.be.null;
+    })
+    it("No error should be returned", function() {
       expect(speechError).to.be.null;
     })
   })
+})
 
+describe("Testing get odometer intent", function () {
+
+  var speechResponse = null;
+  var speechError = null;
+
+  before(function(done) {
+    index.handler(alexaRequest('GetOdometerIntent'), ctx);
+    ctx.Promise
+      .then(response => {
+        speechResponse = response;
+        done();
+      })
+      .catch(error => {
+        speechError = error;
+        done();
+      })
+  })
+
+  describe("Is the endpoint functioning", function() {
+    it("A response should be spoken", function() {
+      expect(speechResponse).to.not.be.null;
+    })
+    it("No error should be returned", function() {
+      expect(speechError).to.be.null;
+    })
+  })
+})
+
+describe("Testing get license plate intent", function () {
+
+  var speechResponse = null;
+  var speechError = null;
+
+  before(function(done) {
+    index.handler(alexaRequest('GetLicensePlateIntent'), ctx);
+    ctx.Promise
+      .then(response => {
+        speechResponse = response;
+        done();
+      })
+      .catch(error => {
+        speechError = error;
+        done();
+      })
+  })
+
+  describe("Is the endpoint functioning", function() {
+    it("A response should be spoken", function() {
+      expect(speechResponse).to.not.be.null;
+    })
+    it("No error should be returned", function() {
+      expect(speechError).to.be.null;
+    })
+  })
+})
+
+describe("Testing unlock doors intent", function () {
+
+  var speechResponse = null;
+  var speechError = null;
+
+  before(function(done) {
+    index.handler(alexaRequest('UnlockDoorsIntent'), ctx);
+    ctx.Promise
+      .then(response => {
+        speechResponse = response;
+        done();
+      })
+      .catch(error => {
+        speechError = error;
+        done();
+      })
+  })
+
+  describe("Is the endpoint functioning", function() {
+    it("A response should be spoken", function() {
+      expect(speechResponse).to.not.be.null;
+    })
+    it("No error should be returned", function() {
+      expect(speechError).to.be.null;
+    })
+  })
+})
+
+describe("Testing get tire pressure intent", function () {
+
+  var speechResponse = null;
+  var speechError = null;
+
+  before(function(done) {
+    index.handler(alexaRequest('GetTirePressureIntent'), ctx);
+    ctx.Promise
+      .then(response => {
+        speechResponse = response;
+        done();
+      })
+      .catch(error => {
+        speechError = error;
+        done();
+      })
+  })
+
+  describe("Is the endpoint functioning", function() {
+    it("A response should be spoken", function() {
+      expect(speechResponse).to.not.be.null;
+    })
+    it("No error should be returned", function() {
+      expect(speechError).to.be.null;
+    })
+  })
+})
+
+describe("Testing get fuel level intent", function () {
+
+  var speechResponse = null;
+  var speechError = null;
+
+  before(function(done) {
+    index.handler(alexaRequest('GetFuelLevelIntent'), ctx);
+    ctx.Promise
+      .then(response => {
+        speechResponse = response;
+        done();
+      })
+      .catch(error => {
+        speechError = error;
+        done();
+      })
+  })
+
+  describe("Is the endpoint functioning", function() {
+    it("A response should be spoken", function() {
+      expect(speechResponse).to.not.be.null;
+    })
+    it("No error should be returned", function() {
+      expect(speechError).to.be.null;
+    })
+  })
+})
+
+describe("Testing check doors intent", function () {
+
+  var speechResponse = null;
+  var speechError = null;
+
+  before(function(done) {
+    index.handler(alexaRequest('CheckDoorsIntent'), ctx);
+    ctx.Promise
+      .then(response => {
+        speechResponse = response;
+        done();
+      })
+      .catch(error => {
+        speechError = error;
+        done();
+      })
+  })
+
+  describe("Is the endpoint functioning", function() {
+    it("A response should be spoken", function() {
+      expect(speechResponse).to.not.be.null;
+    })
+    it("No error should be returned", function() {
+      expect(speechError).to.be.null;
+    })
+  })
 })
