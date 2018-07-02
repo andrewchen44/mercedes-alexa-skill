4c8eeb6e57f8d901f6a4bd2af33a65eaf96c996c
Andrew Chen <andrewchen.c44@gmail.com>
2018-07-01 23:41:45 -0700

    Factor our response messages

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 "use strict";
 const Alexa = require("alexa-sdk");
 
-const HELP_MESSAGE = "You can ask me things like, are my mercedes doors closed, how much fuel does my mercedes have, what is my mercedes odometer, what is my mercedes license plate number, can you lock or unlock the doors of my mercedes, what are the tire pressure of my Mercedes, are the doors of my mercedes closed, or you can say exit... What can I help you with?";
+const APP_ID = "amzn1.ask.skill.32ca19f7-424a-432a-88ee-b20924748589";
+const GREETING_MESSAGE = "Hi, what would you like to know about your Mercedes-Benz? You can say, help me with my mercedes, to hear all possible options";
+const ODOMETER_MESSAGE = "Your Mercedes-Benz odometer is at 3456 miles.";
+const LOCK_DOOR_MESSAGE = 'Locking the doors to your Mercedes-Benz.';
+const LICENSE_PLATE_MESSAGE = 'Your Mercedes-Benz license plate number is 5LK8F9S.';
+const UNLOCK_DOOR_MESSAGE = 'Unlocking the doors to your Mercedes-Benz.';
+const GET_TIRE_PRESSURE_MESSAGE = 'The front left tire is at 250 psi, the front right tire is at 240 psi, the back left tire is at 245 psi, and the back right tire is at 235 psi.';
+const GET_FUEL_LEVEL_MESSAGE = 'The fuel tank of your Mercedes-Benz is 48% full at 8.9 gallon.';
+const CHECK_DOOR_MESSAGE = 'All doors of your Mercedes-Benz are currently closed.';
+const HELP_MESSAGE = "Some things you can do with your mercedes are, ask your mercedes if the doors are open, ask your mercedes the fuel level, your my mercedes for the odometer, ask your mercedes for the license plate, tell you mercedes to lock or unlock the doors, ask your mercedes for the tire pressure, or you can say exit... What can I help you with?";
 const HELP_REPROMPT = "What can I help you with?";
 const STOP_MESSAGE = "Goodbye!";
 
@@ -10,36 +19,35 @@ const handlers = {
     this.emit("GreetingsIntent");
   },
   GreetingsIntent: function() {
-    const speechOutput = "Hi, what would you like to know about your Mercedes-Benz? You can say, help me with my mercedes, to hear all possible options";
-    this.response.speak(speechOutput);
+    this.response.speak(GREETING_MESSAGE);
     this.emit(":responseReady");
   },
   "GetOdometerIntent": function() {
-    this.response.speak("Your Mercedes-Benz odometer is at 3456 miles.");
+    this.response.speak(ODOMETER_MESSAGE);
     this.emit(":responseReady");
   },
   "GetLicensePlateIntent": function() {
-    this.response.speak('Your Mercedes-Benz license plate number is 5LK8F9S.');
+    this.response.speak();
     this.emit(":responseReady");
   },
   "LockDoorsIntent": function() {
-    this.response.speak('Locking the doors to your Mercedes-Benz.');
+    this.response.speak(LOCK_DOOR_MESSAGE);
     this.emit(":responseReady");
   }, 
   "UnlockDoorsIntent": function() {
-    this.response.speak('Unlocking the doors to your Mercedes-Benz.');
+    this.response.speak(UNLOCK_DOOR_MESSAGE);
     this.emit(":responseReady");
   }, 
   "GetTirePressureIntent": function() {
-    this.response.speak('The front left tire is at 250 psi, the front right tire is at 240 psi, the back left tire is at 245 psi, and the back right tire is at 235 psi.');
+    this.response.speak(GET_TIRE_PRESSURE_MESSAGE);
     this.emit(":responseReady");
   }, 
   "GetFuelLevelIntent": function() {
-    this.response.speak('The fuel tank of your Mercedes-Benz is 48% full at 8.9 gallon.');
+    this.response.speak(GET_FUEL_LEVEL_MESSAGE);
     this.emit(":responseReady");
   }, 
   "CheckDoorsIntent": function() {
-    this.response.speak('All doors of your Mercedes-Benz are currently closed.');
+    this.response.speak(CHECK_DOOR_MESSAGE);
     this.emit(":responseReady");
   },  
   "AMAZON.HelpIntent": function() {
@@ -47,18 +55,18 @@ const handlers = {
     this.emit(":responseReady");
   },
   "AMAZON.CancelIntent": function() {
-    this.response.speak('Reqest has been canceled.');
+    this.response.speak(STOP_MESSAGE);
     this.emit(":responseReady");
   },
   "AMAZON.StopIntent": function() {
-    this.response.speak('Request has been stopped');
+    this.response.speak(STOP_MESSAGE);
     this.emit(":responseReady");
   }
 };
 
 exports.handler = function(event, context, callback) {
   const alexa = Alexa.handler(event, context, callback);
-  alexa.appId = "amzn1.ask.skill.32ca19f7-424a-432a-88ee-b20924748589";
+  alexa.appId = APP_ID;
   alexa.registerHandlers(handlers);
   alexa.execute();
 };
